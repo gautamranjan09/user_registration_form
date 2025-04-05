be2abc9f14a5884c894b31daa8f3c75e96123bcd
gautamranjan09 <gautamranjan96@gmail.com>
2025-04-06 05:28:14 +0530

    initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+//import RegistrationForm from "./components/RegistrationForm1"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-rose-50 to-teal-50 p-4">
+      <div className="w-full max-w-md">
+       hello
+      </div>
+    </div>
+  )
+}
+
+export default App;
+
